a3f08b243c2fc6c79cd385547d172c60d6521858
Dan Sorahan <dan@symless.com>
2017-12-06 12:06:00 +0000

    Test DB setup

--- tests/BraintreePaymentsTest.php
+++ tests/BraintreePaymentsTest.php
@@ -19,4 +19,38 @@ class BraintreePaymentsTest extends PHPUnit_Framework_TestCase
 			$dotenv->load();
 		}
 	}
+
+	public function setUp()
+	{
+		Braintree_Configuration::environment('sandbox');
+		Braintree_Configuration::merchantId(getenv('BRAINTREE_MERCHANT_ID'));
+		Braintree_Configuration::publicKey(getenv('BRAINTREE_PUBLIC_KEY'));
+		Braintree_Configuration::privateKey(getenv('BRAINTREE_PRIVATE_KEY'));
+
+		Eloquent::unguard();
+
+		$db = new DB();
+
+		$db->addConnection([
+			'driver'    =>  'sqlite',
+			'database'  => ':memory:',
+		]);
+
+		$db->bootEloquent();
+		$db->setAsGlobal();
+
+		$this->schema()->create('users', function($table) {
+			$table->increments('id');
+			$table->string('email');
+			$table->string('forename');
+			$table->string('surname');
+			$table->string('braintree_id')->nullable();
+			$table->string('paypal_email')->nullable();
+			$table->string('card_brand')->nullable();
+			$table->string('card_last_four')->nullable();
+			$table->timestamps();
+		});
+	}
+
+
 }
